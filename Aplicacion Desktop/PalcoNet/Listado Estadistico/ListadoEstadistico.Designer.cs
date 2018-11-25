9ad17b516acee1881504cff270d481f528c166c8
Nicolas Kim <31737009+nicokim@users.noreply.github.com>
2018-11-25 08:50:49 -0300

    Listado estadistico

--- Aplicacion Desktop/PalcoNet/Listado Estadistico/ListadoEstadistico.Designer.cs
+++ Aplicacion Desktop/PalcoNet/Listado Estadistico/ListadoEstadistico.Designer.cs
@@ -1,4 +1,4 @@
-﻿partial class ListadoEstadistico
+﻿    partial class ListadoEstadistico
     {
         /// <summary>
         /// Required designer variable.
@@ -36,14 +36,21 @@
             this.minimizingLabel = new System.Windows.Forms.Label();
             this.titleLabel = new System.Windows.Forms.Label();
             this.closingLabel = new System.Windows.Forms.Label();
-            this.gradosCombobox = new System.Windows.Forms.ComboBox();
+            this.Combobox_listado = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.listadoActualPublicacion = new System.Windows.Forms.DataGridView();
+            this.listado_estadistico = new System.Windows.Forms.DataGridView();
+            this.primerTrimestre = new System.Windows.Forms.RadioButton();
+            this.segundoTrimestre = new System.Windows.Forms.RadioButton();
+            this.tercerTrimestre = new System.Windows.Forms.RadioButton();
+            this.cuartoTrimestre = new System.Windows.Forms.RadioButton();
+            this.comboAnio = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_listado = new System.Windows.Forms.Button();
             this.sidepanel.SuspendLayout();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.topbar.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.listadoActualPublicacion)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.listado_estadistico)).BeginInit();
             this.SuspendLayout();
             // 
             // sidepanel
@@ -54,7 +61,7 @@
             this.sidepanel.Dock = System.Windows.Forms.DockStyle.Left;
             this.sidepanel.Location = new System.Drawing.Point(0, 0);
             this.sidepanel.Name = "sidepanel";
-            this.sidepanel.Size = new System.Drawing.Size(200, 669);
+            this.sidepanel.Size = new System.Drawing.Size(200, 400);
             this.sidepanel.TabIndex = 0;
             // 
             // label17
@@ -65,9 +72,9 @@
             this.label17.Location = new System.Drawing.Point(4, 56);
             this.label17.MaximumSize = new System.Drawing.Size(190, 190);
             this.label17.Name = "label17";
-            this.label17.Size = new System.Drawing.Size(159, 34);
+            this.label17.Size = new System.Drawing.Size(190, 51);
             this.label17.TabIndex = 52;
-            this.label17.Text = "Puedes ver los distintos listados estadisticos";
+            this.label17.Text = "Usted puede ver los distintos tipos de listados en esta pantalla";
             // 
             // panel1
             // 
@@ -109,7 +116,7 @@
             this.topbar.Dock = System.Windows.Forms.DockStyle.Top;
             this.topbar.Location = new System.Drawing.Point(200, 0);
             this.topbar.Name = "topbar";
-            this.topbar.Size = new System.Drawing.Size(1001, 49);
+            this.topbar.Size = new System.Drawing.Size(945, 49);
             this.topbar.TabIndex = 1;
             // 
             // minimizingLabel
@@ -118,7 +125,7 @@
             this.minimizingLabel.BackColor = System.Drawing.Color.Transparent;
             this.minimizingLabel.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.minimizingLabel.ForeColor = System.Drawing.Color.White;
-            this.minimizingLabel.Location = new System.Drawing.Point(923, 11);
+            this.minimizingLabel.Location = new System.Drawing.Point(868, 9);
             this.minimizingLabel.Name = "minimizingLabel";
             this.minimizingLabel.Size = new System.Drawing.Size(34, 25);
             this.minimizingLabel.TabIndex = 4;
@@ -130,7 +137,7 @@
             this.titleLabel.AutoSize = true;
             this.titleLabel.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.titleLabel.ForeColor = System.Drawing.Color.White;
-            this.titleLabel.Location = new System.Drawing.Point(6, 11);
+            this.titleLabel.Location = new System.Drawing.Point(6, 12);
             this.titleLabel.Name = "titleLabel";
             this.titleLabel.Size = new System.Drawing.Size(189, 24);
             this.titleLabel.TabIndex = 3;
@@ -142,44 +149,135 @@
             this.closingLabel.BackColor = System.Drawing.Color.Transparent;
             this.closingLabel.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.closingLabel.ForeColor = System.Drawing.Color.White;
-            this.closingLabel.Location = new System.Drawing.Point(963, 12);
+            this.closingLabel.Location = new System.Drawing.Point(905, 9);
             this.closingLabel.Name = "closingLabel";
             this.closingLabel.Size = new System.Drawing.Size(26, 25);
             this.closingLabel.TabIndex = 2;
             this.closingLabel.Text = "X";
             this.closingLabel.Click += new System.EventHandler(this.closingLabel_Click);
             // 
-            // gradosCombobox
+            // Combobox_listado
             // 
-            this.gradosCombobox.FormattingEnabled = true;
-            this.gradosCombobox.Location = new System.Drawing.Point(507, 90);
-            this.gradosCombobox.Name = "gradosCombobox";
-            this.gradosCombobox.Size = new System.Drawing.Size(318, 21);
-            this.gradosCombobox.TabIndex = 15;
+            this.Combobox_listado.FormattingEnabled = true;
+            this.Combobox_listado.Location = new System.Drawing.Point(210, 86);
+            this.Combobox_listado.Name = "Combobox_listado";
+            this.Combobox_listado.Size = new System.Drawing.Size(318, 21);
+            this.Combobox_listado.TabIndex = 15;
+            this.Combobox_listado.SelectedIndexChanged += new System.EventHandler(this.Combobox_listado_SelectedIndexChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Black;
-            this.label2.Location = new System.Drawing.Point(523, 66);
+            this.label2.Location = new System.Drawing.Point(206, 56);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(288, 21);
+            this.label2.Size = new System.Drawing.Size(259, 21);
             this.label2.TabIndex = 16;
-            this.label2.Text = "Elija el tipo de listado que quiere ver";
+            this.label2.Text = "Elija el tipo de listado estadistico";
             // 
-            // listadoActualPublicacion
+            // listado_estadistico
             // 
-            this.listadoActualPublicacion.AllowUserToAddRows = false;
-            this.listadoActualPublicacion.AllowUserToDeleteRows = false;
-            this.listadoActualPublicacion.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.listadoActualPublicacion.Location = new System.Drawing.Point(305, 117);
-            this.listadoActualPublicacion.MultiSelect = false;
-            this.listadoActualPublicacion.Name = "listadoActualPublicacion";
-            this.listadoActualPublicacion.ReadOnly = true;
-            this.listadoActualPublicacion.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.listadoActualPublicacion.Size = new System.Drawing.Size(742, 529);
-            this.listadoActualPublicacion.TabIndex = 17;
+            this.listado_estadistico.AllowUserToAddRows = false;
+            this.listado_estadistico.AllowUserToDeleteRows = false;
+            this.listado_estadistico.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.listado_estadistico.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCellsExceptHeaders;
+            this.listado_estadistico.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.listado_estadistico.Location = new System.Drawing.Point(210, 126);
+            this.listado_estadistico.MultiSelect = false;
+            this.listado_estadistico.Name = "listado_estadistico";
+            this.listado_estadistico.ReadOnly = true;
+            this.listado_estadistico.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.listado_estadistico.Size = new System.Drawing.Size(921, 127);
+            this.listado_estadistico.TabIndex = 17;
+            // 
+            // primerTrimestre
+            // 
+            this.primerTrimestre.AutoSize = true;
+            this.primerTrimestre.Enabled = false;
+            this.primerTrimestre.Location = new System.Drawing.Point(685, 86);
+            this.primerTrimestre.Name = "primerTrimestre";
+            this.primerTrimestre.Size = new System.Drawing.Size(100, 17);
+            this.primerTrimestre.TabIndex = 18;
+            this.primerTrimestre.TabStop = true;
+            this.primerTrimestre.Text = "Primer Trimestre";
+            this.primerTrimestre.UseVisualStyleBackColor = true;
+            this.primerTrimestre.CheckedChanged += new System.EventHandler(this.primerTrimestre_CheckedChanged);
+            // 
+            // segundoTrimestre
+            // 
+            this.segundoTrimestre.AutoSize = true;
+            this.segundoTrimestre.Enabled = false;
+            this.segundoTrimestre.Location = new System.Drawing.Point(790, 86);
+            this.segundoTrimestre.Name = "segundoTrimestre";
+            this.segundoTrimestre.Size = new System.Drawing.Size(114, 17);
+            this.segundoTrimestre.TabIndex = 19;
+            this.segundoTrimestre.TabStop = true;
+            this.segundoTrimestre.Text = "Segundo Trimestre";
+            this.segundoTrimestre.UseVisualStyleBackColor = true;
+            this.segundoTrimestre.CheckedChanged += new System.EventHandler(this.segundoTrimestre_CheckedChanged);
+            // 
+            // tercerTrimestre
+            // 
+            this.tercerTrimestre.AutoSize = true;
+            this.tercerTrimestre.Enabled = false;
+            this.tercerTrimestre.Location = new System.Drawing.Point(910, 86);
+            this.tercerTrimestre.Name = "tercerTrimestre";
+            this.tercerTrimestre.Size = new System.Drawing.Size(102, 17);
+            this.tercerTrimestre.TabIndex = 20;
+            this.tercerTrimestre.TabStop = true;
+            this.tercerTrimestre.Text = "Tercer Trimestre";
+            this.tercerTrimestre.UseVisualStyleBackColor = true;
+            this.tercerTrimestre.CheckedChanged += new System.EventHandler(this.tercerTrimestre_CheckedChanged);
+            // 
+            // cuartoTrimestre
+            // 
+            this.cuartoTrimestre.AutoSize = true;
+            this.cuartoTrimestre.Enabled = false;
+            this.cuartoTrimestre.Location = new System.Drawing.Point(1018, 86);
+            this.cuartoTrimestre.Name = "cuartoTrimestre";
+            this.cuartoTrimestre.Size = new System.Drawing.Size(102, 17);
+            this.cuartoTrimestre.TabIndex = 21;
+            this.cuartoTrimestre.TabStop = true;
+            this.cuartoTrimestre.Text = "Cuarto Trimestre";
+            this.cuartoTrimestre.UseVisualStyleBackColor = true;
+            this.cuartoTrimestre.CheckedChanged += new System.EventHandler(this.cuartoTrimestre_CheckedChanged);
+            // 
+            // comboAnio
+            // 
+            this.comboAnio.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboAnio.Enabled = false;
+            this.comboAnio.FormattingEnabled = true;
+            this.comboAnio.Location = new System.Drawing.Point(605, 85);
+            this.comboAnio.Name = "comboAnio";
+            this.comboAnio.Size = new System.Drawing.Size(74, 21);
+            this.comboAnio.TabIndex = 22;
+            this.comboAnio.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(564, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 17);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "Año:";
+            // 
+            // btn_listado
+            // 
+            this.btn_listado.BackColor = System.Drawing.Color.PaleGreen;
+            this.btn_listado.Enabled = false;
+            this.btn_listado.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_listado.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_listado.Location = new System.Drawing.Point(650, 289);
+            this.btn_listado.Name = "btn_listado";
+            this.btn_listado.Size = new System.Drawing.Size(481, 67);
+            this.btn_listado.TabIndex = 49;
+            this.btn_listado.Text = "Obtener listado!";
+            this.btn_listado.UseVisualStyleBackColor = false;
+            this.btn_listado.Click += new System.EventHandler(this.btn_guardar_Click);
             // 
             // ListadoEstadistico
             // 
@@ -187,11 +285,18 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.BackColor = System.Drawing.Color.Gainsboro;
-            this.ClientSize = new System.Drawing.Size(1201, 669);
+            this.ClientSize = new System.Drawing.Size(1145, 400);
             this.ControlBox = false;
-            this.Controls.Add(this.listadoActualPublicacion);
+            this.Controls.Add(this.btn_listado);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboAnio);
+            this.Controls.Add(this.cuartoTrimestre);
+            this.Controls.Add(this.tercerTrimestre);
+            this.Controls.Add(this.segundoTrimestre);
+            this.Controls.Add(this.primerTrimestre);
+            this.Controls.Add(this.listado_estadistico);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.gradosCombobox);
+            this.Controls.Add(this.Combobox_listado);
             this.Controls.Add(this.topbar);
             this.Controls.Add(this.sidepanel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -207,7 +312,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.topbar.ResumeLayout(false);
             this.topbar.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.listadoActualPublicacion)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.listado_estadistico)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -224,8 +329,15 @@
         private System.Windows.Forms.Label titleLabel;
         private System.Windows.Forms.Label minimizingLabel;
         private System.Windows.Forms.Label label17;
-        private System.Windows.Forms.ComboBox gradosCombobox;
+        private System.Windows.Forms.ComboBox Combobox_listado;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.DataGridView listadoActualPublicacion;
+        private System.Windows.Forms.DataGridView listado_estadistico;
+        private System.Windows.Forms.RadioButton primerTrimestre;
+        private System.Windows.Forms.RadioButton segundoTrimestre;
+        private System.Windows.Forms.RadioButton tercerTrimestre;
+        private System.Windows.Forms.RadioButton cuartoTrimestre;
+        private System.Windows.Forms.ComboBox comboAnio;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_listado;
 
     }
--- Aplicacion Desktop/PalcoNet/Listado Estadistico/ListadoEstadistico.cs
+++ Aplicacion Desktop/PalcoNet/Listado Estadistico/ListadoEstadistico.cs
@@ -11,14 +11,25 @@ using System.Windows.Forms;
 
     public partial class ListadoEstadistico : Form
     {
-        public ListadoEstadistico(publicacion publicacionAUsar)
+        public String anio{get;set;}
+        public String mes_dia_inicio { get; set; }
+        public String mes_dia_fin { get; set; }
+        public ListadoEstadistico()
         {
             InitializeComponent();
-            this.publicacionAUsar = publicacionAUsar;
         }
         private void ListadoEstadistico_Load(object sender, EventArgs e)
         {
-
+            Combobox_listado.Items.Add("Empresas con mayor cantidad de localidades no vendidas");
+            Combobox_listado.Items.Add("Clientes con mayores puntos vencidos");
+            Combobox_listado.Items.Add("Clientes con mayor cantidad de compras");
+            mes_dia_inicio = "01/01";
+            mes_dia_fin = "03/31";
+            for (int i = 1950; i <= 2050; i++)
+            {
+                comboAnio.Items.Add(i.ToString());
+            }
+            MessageBox.Show(this.anio);
         }
         // controles de cualquier form
         private void closingLabel_Click(object sender, EventArgs e)
@@ -40,14 +51,60 @@ using System.Windows.Forms;
         private const int WM_NCHITTEST = 0x84;
         private const int HT_CLIENT = 0x1;
         private const int HT_CAPTION = 0x2;
-        public publicacion publicacionAUsar{get;set;}
 
-        private void btn_seleccionar_Click(object sender, EventArgs e)
+        private void Combobox_listado_SelectedIndexChanged(object sender, EventArgs e)
         {
-            grado gradoSeleccionado = (grado)gradosCombobox.SelectedItem;
-            this.publicacionAUsar.id_grado = gradoSeleccionado.id_grado;
-            BDManager.updateSet("publicacion","id_publicacion", publicacionAUsar);
-            MessageBox.Show("Se modifico el grado", "Grado de la publicacion modificado correctamente", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            this.Close();
+            comboAnio.Enabled = true;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.anio = (String)comboAnio.SelectedItem;
+            primerTrimestre.Enabled = true;
+            segundoTrimestre.Enabled = true;
+            tercerTrimestre.Enabled = true;
+            cuartoTrimestre.Enabled = true;
+            primerTrimestre.Checked = true;
+            btn_listado.Enabled = true;
+        }
+
+        private void segundoTrimestre_CheckedChanged(object sender, EventArgs e)
+        {
+            mes_dia_inicio = "04/01";
+            mes_dia_fin = "06/30";
+        }
+
+        private void tercerTrimestre_CheckedChanged(object sender, EventArgs e)
+        {
+            mes_dia_inicio = "07/01";
+            mes_dia_fin = "09/30";
+        }
+
+        private void cuartoTrimestre_CheckedChanged(object sender, EventArgs e)
+        {
+            mes_dia_inicio = "10/30";
+            mes_dia_fin = "12/31";
+        }
+
+        private void primerTrimestre_CheckedChanged(object sender, EventArgs e)
+        {
+            mes_dia_inicio = "01/01";
+            mes_dia_fin = "03/31";
+        }
+
+        private void btn_guardar_Click(object sender, EventArgs e)
+        {
+            switch ((String)Combobox_listado.SelectedItem)
+            {
+
+                case "Empresas con mayor cantidad de localidades no vendidas":
+                    listado_estadistico.DataSource = BDManager.getData("SELECT TOP 5 p.username,cuit,estado visibilidad,COUNT(*) cantidad FROM EQUISDE.publicacion p JOIN EQUISDE.empresa e ON(p.username = e.username) JOIN EQUISDE.grado g ON(g.id_grado=p.id_grado) JOIN EQUISDE.ubicacion u ON (p.id_publicacion=u.id_publicacion) LEFT JOIN EQUISDE.compra_x_ubicacion cu ON (u.id_ubicacion = cu.id_ubicacion)  WHERE cu.id_compra IS NULL AND p.fecha_publicacion BETWEEN CAST('" + anio + "/" + mes_dia_inicio + "' AS datetime) AND CAST('" + anio + "/" + mes_dia_fin + "' AS datetime) GROUP BY p.username,cuit,razon_social,estado,fecha_publicacion,p.id_grado,p.id_publicacion ORDER BY  cantidad DESC,fecha_publicacion,p.id_grado ASC");
+                    break;
+                case "Clientes con mayores puntos vencidos":
+                    listado_estadistico.DataSource = BDManager.getData("SELECT nombre,apellido,COUNT(id_compra)cantidad FROM EQUISDE.cliente cl JOIN EQUISDE.compra cp ON (cl.username = cp.username) WHERE cp.fecha_vencimiento_puntos BETWEEN CAST('" + anio + "/" + mes_dia_inicio + "' AS datetime) AND CAST('" + anio + "/" + mes_dia_fin + "' AS datetime) GROUP BY nombre,apellido");
+                    break;
+                case "Clientes con mayor cantidad de compras":
+                    break;
+            }
         }
 }
