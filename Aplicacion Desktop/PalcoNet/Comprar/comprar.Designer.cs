9c62814cd411e314ff1d98c836948f6b861043b6
Nicolas Kim <31737009+nicokim@users.noreply.github.com>
2018-11-25 02:17:39 -0300

    Comrpas ui

---
+++ Aplicacion Desktop/PalcoNet/Comprar/comprar.Designer.cs
@@ -0,0 +1,187 @@
+﻿partial class comprar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(comprar));
+            this.sidepanel = new System.Windows.Forms.Panel();
+            this.label17 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.title = new System.Windows.Forms.Label();
+            this.topbar = new System.Windows.Forms.Panel();
+            this.minimizingLabel = new System.Windows.Forms.Label();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.closingLabel = new System.Windows.Forms.Label();
+            this.sidepanel.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.topbar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // sidepanel
+            // 
+            this.sidepanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(53)))), ((int)(((byte)(65)))));
+            this.sidepanel.Controls.Add(this.label17);
+            this.sidepanel.Controls.Add(this.panel1);
+            this.sidepanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.sidepanel.Location = new System.Drawing.Point(0, 0);
+            this.sidepanel.Name = "sidepanel";
+            this.sidepanel.Size = new System.Drawing.Size(200, 617);
+            this.sidepanel.TabIndex = 0;
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label17.ForeColor = System.Drawing.Color.White;
+            this.label17.Location = new System.Drawing.Point(4, 56);
+            this.label17.MaximumSize = new System.Drawing.Size(190, 190);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(165, 34);
+            this.label17.TabIndex = 51;
+            this.label17.Text = "Usted puede realizar sus compras aqui";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(146)))), ((int)(((byte)(29)))));
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.title);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 49);
+            this.panel1.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(166, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(31, 32);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.Location = new System.Drawing.Point(3, 12);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(162, 24);
+            this.title.TabIndex = 2;
+            this.title.Text = "PalcoNet FRBA";
+            // 
+            // topbar
+            // 
+            this.topbar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(53)))), ((int)(((byte)(65)))));
+            this.topbar.Controls.Add(this.minimizingLabel);
+            this.topbar.Controls.Add(this.titleLabel);
+            this.topbar.Controls.Add(this.closingLabel);
+            this.topbar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.topbar.Location = new System.Drawing.Point(200, 0);
+            this.topbar.Name = "topbar";
+            this.topbar.Size = new System.Drawing.Size(1251, 49);
+            this.topbar.TabIndex = 1;
+            // 
+            // minimizingLabel
+            // 
+            this.minimizingLabel.AutoSize = true;
+            this.minimizingLabel.BackColor = System.Drawing.Color.Transparent;
+            this.minimizingLabel.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minimizingLabel.ForeColor = System.Drawing.Color.White;
+            this.minimizingLabel.Location = new System.Drawing.Point(1181, 12);
+            this.minimizingLabel.Name = "minimizingLabel";
+            this.minimizingLabel.Size = new System.Drawing.Size(34, 25);
+            this.minimizingLabel.TabIndex = 4;
+            this.minimizingLabel.Text = "__";
+            this.minimizingLabel.Click += new System.EventHandler(this.minimizingLabel_Click);
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Location = new System.Drawing.Point(6, 12);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(102, 24);
+            this.titleLabel.TabIndex = 3;
+            this.titleLabel.Text = "Comprar";
+            // 
+            // closingLabel
+            // 
+            this.closingLabel.AutoSize = true;
+            this.closingLabel.BackColor = System.Drawing.Color.Transparent;
+            this.closingLabel.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.closingLabel.ForeColor = System.Drawing.Color.White;
+            this.closingLabel.Location = new System.Drawing.Point(1213, 12);
+            this.closingLabel.Name = "closingLabel";
+            this.closingLabel.Size = new System.Drawing.Size(26, 25);
+            this.closingLabel.TabIndex = 2;
+            this.closingLabel.Text = "X";
+            this.closingLabel.Click += new System.EventHandler(this.closingLabel_Click);
+            // 
+            // comprar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(1451, 617);
+            this.ControlBox = false;
+            this.Controls.Add(this.topbar);
+            this.Controls.Add(this.sidepanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "comprar";
+            this.Text = "MainWindow";
+            this.Load += new System.EventHandler(this.comprar_Load);
+            this.sidepanel.ResumeLayout(false);
+            this.sidepanel.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.topbar.ResumeLayout(false);
+            this.topbar.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel sidepanel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Panel topbar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label closingLabel;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label minimizingLabel;
+        private System.Windows.Forms.Label label17;
+
+    }
---
+++ Aplicacion Desktop/PalcoNet/Comprar/comprar.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+    public partial class comprar : Form
+    {
+        public comprar()
+        {
+            InitializeComponent();
+        }
+        private void comprar_Load(object sender, EventArgs e)
+        {
+
+        }
+        // controles de cualquier form
+        private void closingLabel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void minimizingLabel_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+        protected override void WndProc(ref Message m) // PARA QUE SE PUEDA HACER DRAGGING DEL FORM
+        {
+            base.WndProc(ref m);
+            if (m.Msg == WM_NCHITTEST)
+                m.Result = (IntPtr)(HT_CAPTION);
+        }
+
+        private const int WM_NCHITTEST = 0x84;
+        private const int HT_CLIENT = 0x1;
+        private const int HT_CAPTION = 0x2;
+    }
